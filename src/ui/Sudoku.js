d2b0e744cbcf3008254bfb3aa6060bd941cda622
LQ Hoang <longquanhp@gmail.com>
2018-06-19 01:01:58 -0400

    change how we init the grid, and created shuffle function that shuffles the values of the grid

--- src/ui/Sudoku.js
+++ src/ui/Sudoku.js
@@ -1,45 +1,60 @@
 import React, { Component } from 'react'
 import styled from 'styled-components'
 
+const MAX_LENGTH = 9
 export default class Sudoku extends Component {
   constructor() {
     super()
     this.state = {
       grid: [],
     }
-
-    this.fillGrid = this.fillGrid.bind(this)
+    this.initializeGrid = this.initializeGrid.bind(this)
+    this.shuffleGrid = this.shuffleGrid.bind(this)
   }
 
   componentWillMount() {
-    this.fillGrid()
+    const { grid } = this.state
+    this.initializeGrid()
+    this.shuffleGrid(grid)
   }
 
-  fillGrid() {
+  initializeGrid() {
     const { grid } = this.state
-    for (let i = 0; i < 9; i++) {
-      grid.push(Array(9))
+    for (let i = 0; i < MAX_LENGTH; i++) {
+      grid.push([1,2,3,4,5,6,7,8,9])
     }
-    for (let row = 0; row < 9; row++) {
-      for (let column = 0; column < 9; column++) {
-        grid[row][column] = this.getRandomNumber()
+  }
+
+  shuffleGrid(grid) {
+    // Using Fisher-Yates shuffle algorithm
+    let shuffledGrid = grid
+    let gridLength = shuffledGrid.length-1
+    for (let row = gridLength; row >= 0; row--) {
+      for (let column = gridLength; column >= 0; column--) {
+        let cell = Math.floor(Math.random() * column)
+        let temp = shuffledGrid[row][cell]
+        shuffledGrid[row][cell] = shuffledGrid[row][column]
+        shuffledGrid[row][column] = temp
       }
     }
-  }
-
-  getRandomNumber() {
-    return Math.floor(Math.random() * (9)) + 1
+    this.setState({
+      grid: shuffledGrid,
+    })
   }
 
   render() {
     return <div>
       <Header>Sudoku</Header>
-      {this.state.grid.map((row) => {
+      {this.state.grid.map((row, index) => {
         return (
-          <div>
-            {row.map((cell) => {
+          <div key={`row-${index}`}>
+            {row.map((cell, index) => {
               return (
-                <Cell>{cell}</Cell>
+                <Cell className="sudoku-cell"
+                      type="number"
+                      key={`column-${index}`}
+                      value={cell}
+                      disabled={cell} />
               )
             })}
         </div>
@@ -55,10 +70,16 @@ const Header = styled.h1`
   letter-spacing: 3px;
 `
 
-const Cell = styled.div`
-  padding: 10px 15px;
-  width: fit-content;
-  display: inline-block;
-  border: 1px solid rgba(0,0,0,0.1);
-  background-color: white;
+const Cell = styled.input`
+  &.sudoku-cell {
+    width: 27px;
+    height: 40px;
+    text-align: center;
+    display: inline-block;
+    border: 1px solid rgba(0,0,0,0.1);
+    background-color: white;
+    font-family: 'Cousine', serif;
+    font-size: 1rem;
+    padding-left: 13px;
+  }
 `
